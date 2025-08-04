32f8b02c7e7de8c787a70a451a2f795bd5478120
ntt-mikeias-silva <mikeias.silva@netturbo.com.br>
2025-08-04 13:31:28 -0300

    SSH Teste

--- get-asn-clientes.sh
+++ get-asn-clientes.sh
@@ -3,4 +3,4 @@
 ASN=${1}
 COMMAND="dis bgp all summary | i ${ASN}"
 
-timeout 10s sshpass -p ${SSHPASS} ssh zabbix@"${ip_gerencia}" -p 33033 -o KexAlgorithms=+diffie-hellman-group-exchange-sha1 -o StrictHostKeyChecking=no -x "${COMMAND}" 2>/dev/null
+sshpass -p ${SSHPASS} ssh zabbix@"${ip_gerencia}" -p 33033 -o KexAlgorithms=+diffie-hellman-group-exchange-sha1 -o StrictHostKeyChecking=no -x "${COMMAND}" 2>/dev/null
